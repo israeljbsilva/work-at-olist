1146204a85469d45e62567cab8d5575134d88a61
israel.silva <israel.silva@nexxera.com>
2019-08-15 15:14:00 -0300

    Including TelephoneBill model

--- work_at_olist/models.py
+++ work_at_olist/models.py
@@ -3,6 +3,8 @@ import uuid
 from django.db import models
 from django.db.models.fields import CharField, UUIDField
 
+from decimal import Decimal
+
 
 class AbstractCallRecord(models.Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, serialize=False, editable=False, unique=True)
@@ -29,3 +31,18 @@ class CallStartRecord(AbstractCallRecord, models.Model):
         db_table = 'CallStartRecord'
         verbose_name = 'call_start_record'
         verbose_name_plural = 'call_start_records'
+
+
+class TelephoneBill(models.Model):
+    call_id = UUIDField(primary_key=True, default=uuid.uuid4, serialize=False, editable=False, unique=True)
+    destination = CharField('DESTINATION', max_length=11, null=False)
+    call_start_date = models.DateTimeField('CALL_START_DATE', null=False)
+    call_start_time = models.TimeField('CALL_START_TIME', null=False)
+    call_duration = models.TimeField('CALL_DURATION', null=False)
+    call_price = models.DecimalField(decimal_places=2, max_digits=17, default=Decimal("0.0"))
+    source = CharField('SOURCE', max_length=11, null=False)
+
+    class Meta:
+        db_table = 'TelephoneBill'
+        verbose_name = 'telephone_bill'
+        verbose_name_plural = 'telephone_bills'
